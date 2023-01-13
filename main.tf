7b7428c33ffd18319bb0ae1421ccdbc3636e3f8a
WilliamDanielQuinones <45363476+WilliamDanielQuinones@users.noreply.github.com>
2023-01-13 02:02:54 -0500

    update env vars

--- main.tf
+++ main.tf
@@ -37,7 +37,28 @@ resource "aws_ecs_task_definition" "service_definition" {
           "awslogs-stream-prefix" = "${var.service_name}"
         }
       }
-      environment = var.environment_variables
+      environment = concat(var.environment_variables, [
+        {
+          name: "DATABASE_HOST",
+          value: module.db.this_db_instance_address
+        },
+        {
+          name: "DATABASE_PORT",
+          value: module.db.this_db_instance_port
+        },
+        {
+          name: "DATABASE_NAME",
+          value: module.db.this_db_instance_name
+        },
+        {
+          name: "DATABASE_USERNAME",
+          value: module.db.this_db_instance_username
+        },
+        {
+          name: "DATABASE_PASSWORD",
+          value: module.db.this_db_instance_password,
+        },
+      ])
     }
   ])
 
